88def656ec0837f0a6fe8b6b4e4d06cac62d91cb
Eduardo Almeida <duduf2005@hotmail.com>
2023-01-22 17:30:32 -0300

    feat(server): routes post habit + get day

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,13 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from "./routes/_public.routes";
+
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/', async () => {
-  const habits = await prisma.habit.findMany({ 
-    // where: {}
-  })
-  return habits
-})
 
 app.listen({
   port: 3333,
