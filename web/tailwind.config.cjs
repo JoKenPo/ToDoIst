f1b2b53ba27f550ec229e0c5a4749c592c6aa9fc
Eduardo Almeida <duduf2005@hotmail.com>
2023-01-22 18:39:08 -0300

    feat(web): components Head Days Table

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.tsx',
-    './index.html'
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#09090a',
+      },
+      fontFamily: {
+        regular: 'Inter_400Regular',
+        semibold: 'Inter_600SemiBold',
+        bold: 'Inter_700Bold',
+        extrabold: 'Inter_800ExtraBold'
+      },
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))'
+      }
+    },
   },
   plugins: [],
 }
